e3310206d46f97f5526e3899ad7faffcf3cf1119
Claudia Chacon <claudiachacon@claudias-air.lan>
2021-09-01 20:15:05 -0500

    timer trouble

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let answerBtn = document.querySelector(".buttons")
 const difficulty = document.querySelector("#difficulty")
 const category = document.querySelector("#category")
 const message = document.querySelector(".message-container")
-const questionsDisplay = document.querySelector("#questions")
+const questionsDisplay = document.querySelector(".questions-container")
 const scoreCount = document.querySelector("#score")
 const timer = document.querySelector("#timer")
+let time = 15
 let score = 0
 let shuffleQuestions
 let currentQuestion = 0
@@ -17,11 +18,9 @@ document.querySelector("#start").addEventListener("click", function(){
    document.querySelector("#section1").className = "fade-out"
    document.querySelector("#section2").className = "slide-in"
 
-   let time = 15
-
    setTimeout(() => {
-      let startTimer = setInterval(function function1(){
-      timer.innerText = time
+      startTimer = setInterval(function startTime(){
+         timer.innerText = time
          time -=1
          if(time < 0){
             nextQuestion()
@@ -29,16 +28,53 @@ document.querySelector("#start").addEventListener("click", function(){
          }
       }, 1000)
    }, 2000)
+   // answerBtn.addEventListener("click", () => {
+   //    clearTimeout(startTimer)
+   //    timer.innerText = 15
+   //    setTimeout(() => {
+   //       startTimer = setInterval(function startTime(){
+   //          time -=1
+   //          if(time < 0){
+   //             nextQuestion()
+   //             time = 15  
+   //          }
+   //       }, 1000)
+   //    }, 2000)
+   // })
+})
 
 
-     
+answerBtn.addEventListener("click", () => {
+   clearTimeout(startTimer)
+   timer.innerText = 15
+   setTimeout(() => {
+      startTimer = setInterval(function (){
+         time -=1
+         if(time < 0){
+            nextQuestion()
+            time = 15  
+         }
+      }, 1000)
+   }, 2000)
+})
 
-   console.log(timer);
-});
+// function stopTimer() {
+//    setTimeout(() => {
+//       timer.innerText = 15
+//    }, 2000)
+   // // clearTimeout(startTimer)
+   // timer.innerText = 15
 
-// grab data from api:done
-// select random question from data & display result:done
-// select answers(correct/incorrect) from question:done
+   // let startTimer = setInterval(function startTime(){
+   //    timer.innerText = time
+   //       time -=1
+   //       if(time < 0){
+   //          nextQuestion()
+   //          time = 15  
+   //       }
+   //    }, 1000)
+// }
+
 async function fetchData(results) {
    try {
       let res = await axios.get(url)
@@ -64,6 +100,9 @@ function nextQuestion() {
    questionsDisplay.innerText = ""
    resetAnswer()
    playGame(questions)
+   if([currentQuestion] > 10) {
+      endGame()
+   }
 }
 
 function resetAnswer() {
@@ -75,7 +114,10 @@ function resetAnswer() {
 }
 
 function displayQuestion(question) {
-   questionsDisplay.innerText = question
+   let questionText = document.createElement("h2")
+   questionText.classList.add("questions")
+   questionText.innerText = question
+   questionsDisplay.append(questionText)
 }
 
 function displayAnswer(answer) {
@@ -96,7 +138,6 @@ function getAnswer(correct_answer, incorrect_answers) {
             setTimeout(() => {
                nextQuestion()
                scoreCounter()
-               // startTime()
             }, 1000)
          })
          answerBtn.append(button)
@@ -108,7 +149,6 @@ function getAnswer(correct_answer, incorrect_answers) {
             button.classList.add("wrong")
             setTimeout(() => {
                nextQuestion()
-               // resetTime()
             }, 1000)
          })
          answerBtn.append(button)
@@ -127,20 +167,28 @@ function shuffle(array) {
      currentIndex--;
  
      // And swap it with the current element.
-     [array[currentIndex], array[randomIndex]] = [
-       array[randomIndex], array[currentIndex]];
+     [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
    }
  
    return array;
- }
+}
 
 function scoreCounter() {
    score++
    scoreCount.innerText = score
    if(score > 10) {
+      resetTimer()
      let win = document.createElement("h2")
       win.classList.add("message")
       win.textContent = "Perfect score!"
       message.append(win)
    }
- }
+}
+
+function endGame() {
+   stopTimer()
+      let gameOver = document.createElement("h2")
+      gameOver.classList.add("message")
+      gameOver.textContent = "Game over!"
+      message.append(win)
+}
